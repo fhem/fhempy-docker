cb5870884cd6929e22412cd142a44a36d1756703
sidey79 <7968127+sidey79@users.noreply.github.com>
2022-09-25 23:36:47 +0200

    Add healtcheck script

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,11 @@
 FROM python:3.9.14
 
 WORKDIR /usr/src/app
+COPY src/health-check.sh /health-check.sh
 
 RUN apt update && apt install dbus python-dbus-dev rustc -y
 RUN pip install --no-cache-dir fhempy
+
+HEALTHCHECK --interval=50s --timeout=15s --start-period=10s --retries=3 CMD /health-check.sh
 
 CMD [ "fhempy" ]
---
+++ src/health-check.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+STATE=0
+WEBPORT=15733
+FHEMPY_STATE=$( curl \
+                  --silent \
+                  --insecure \
+                  --output /dev/null \
+                  --write-out "%{http_code}" \
+                  --user-agent 'fhempy-docker/1.0 Health Check' \
+                  "${PROTO}://{$HOSTNAME}:${WEBPORT}/healtcheck" )
+if [ $? -ne 0 ] ||
+   [ -z "${FHEMPY_STATE}" ] ||
+   [ "${FHEMPY_STATE}" == "000" ] ||
+   [ "${FHEMPY_STATE:0:1}" == "5" ]; then
+  RETURN="${HOSTNAME}:(${WEBPORT}): FAILED"
+  STATE=1
+else
+  RETURN="${HOSTNAME}:(${WEBPORT}): OK"
+fi
+
+echo -n ${RETURN}
+exit ${STATE}
